1197d21819d029b52d609d12427b62bc9c094960
Shifu Chen <chen@haplox.com>
2015-12-18 17:42:35 +0800

    Add Sequence/Quality string functions

--- src/Base/Types/quality.jl
+++ src/Base/Types/quality.jl
@@ -2,3 +2,10 @@
 type Quality
 	qual::ASCIIString
 end
+
+Base.length(q::Quality) = length(q.qual)
+Base.sizeof(q::Quality) = sizeof(q.qual)
+Base.getindex(q::Quality, i::Int64) = getindex(q.qual, i)
+#Base.getindex(q::Quality, r::Array{T,1}) = getindex(q.qual, r)
+Base.getindex(q::Quality, r::UnitRange{Int64}) = Quality(getindex(q.qual, r))
+Base.getindex(q::Quality, indx::AbstractArray{Int64,1}) = Quality(getindex(q.qual, indx))
--- src/Base/Types/sequence.jl
+++ src/Base/Types/sequence.jl
@@ -3,3 +3,10 @@
 type Sequence
 	seq::ASCIIString
 end
+
+Base.length(s::Sequence) = length(s.seq)
+Base.sizeof(s::Sequence) = sizeof(s.seq)
+Base.getindex(s::Sequence, i::Int64) = getindex(s.seq, i)
+#Base.getindex(s::Sequence, r::Array{T,1}) = getindex(s.seq, r)
+Base.getindex(s::Sequence, r::UnitRange{Int64}) = Sequence(getindex(s.seq, r))
+Base.getindex(s::Sequence, indx::AbstractArray{Int64,1}) = Sequence(getindex(s.seq, indx))
