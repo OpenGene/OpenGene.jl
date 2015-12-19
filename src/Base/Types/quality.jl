748fd6e7c77be0feafe433354dd276699415ff68
Shifu Chen <chen@haplox.com>
2015-12-20 07:55:38 +0800

    add tests for FastqRead/FastaRead

--- src/Base/Types/quality.jl
+++ src/Base/Types/quality.jl
@@ -9,3 +9,5 @@ Base.getindex(q::Quality, i::Int64) = getindex(q.qual, i)
 Base.getindex(q::Quality, r::UnitRange{Int64}) = Quality(getindex(q.qual, r))
 Base.getindex(q::Quality, indx::AbstractArray{Int64,1}) = Quality(getindex(q.qual, indx))
 Base.reverse(q::Quality) = Quality(reverse(q.qual))
+==(s1::Quality, s2::Quality) = s1.qual == s2.qual
+-(s1::Quality) = Quality(reverse(s1.qual))
--- test/Base/Type/Type.jl
+++ test/Base/Type/Type.jl
@@ -6,6 +6,12 @@ reverse_seq = Sequence("GGGGCCCCTTTTAAAAGGGGCCCCTTTTAAAA")
 compelment_seq = Sequence("TTTTAAAAGGGGCCCCTTTTAAAAGGGGCCCC") 
 reverse_compelment_seq = Sequence("CCCCGGGGAAAATTTTCCCCGGGGAAAATTTT")
 qual = Quality("FFFF####AAAAFFFFFFFFFFFFFFFFFFFF")
+seq_name = "this_is_a_sequence_name"
+fq = FastqRead(seq_name, seq, "+", qual)
+reverse_compelment_fq = FastqRead(seq_name, reverse_compelment_seq, "+", reverse(qual))
+fa = FastaRead(seq_name, seq)
+reverse_compelment_fa = FastaRead(seq_name, reverse_compelment_seq)
+
 @test  seq == same_seq
 @test seq == -reverse_seq
 @test seq == !compelment_seq
@@ -15,3 +21,6 @@ qual = Quality("FFFF####AAAAFFFFFFFFFFFFFFFFFFFF")
 @test seq == ~(~seq)
 @test reverse_compelment_seq == !(-seq)
 @test reverse_compelment_seq == -(!seq)
+@test reverse_compelment_fq.sequence == (~fq).sequence
+@test reverse_compelment_fq.quality == (~fq).quality
+@test reverse_compelment_fa.sequence == (~fa).sequence
