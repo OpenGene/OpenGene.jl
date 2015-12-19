d01bda51e99fce30c45d3492dff3317529cddd89
Shifu Chen <chen@haplox.com>
2015-12-19 23:18:01 +0800

    update tests

--- test/Base/Type/Type.jl
+++ test/Base/Type/Type.jl
@@ -1,7 +1,17 @@
 
 #compare sequence
-@test Sequence("AAAATTTTCCCCGGGG") == Sequence("AAAATTTTCCCCGGGG")
-@test Sequence("AAAATTTTCCCCGGGG") == -Sequence("GGGGCCCCTTTTAAAA")
-@test Sequence("AAAATTTTCCCCGGGG") == -(-(Sequence("AAAATTTTCCCCGGGG")))
-@test Sequence("AAAATTTTCCCCGGGG", DNA_SEQ) != Sequence("AAAATTTTCCCCGGGG", RNA_SEQ)
-@test Sequence("AAAATTTTCCCCGGGG") == ~(~(Sequence("AAAATTTTCCCCGGGG")))
+seq = Sequence("AAAATTTTCCCCGGGGAAAATTTTCCCCGGGG")
+same_seq = Sequence("AAAATTTTCCCCGGGGAAAATTTTCCCCGGGG")
+reverse_seq = Sequence("GGGGCCCCTTTTAAAAGGGGCCCCTTTTAAAA") 
+compelment_seq = Sequence("TTTTAAAAGGGGCCCCTTTTAAAAGGGGCCCC") 
+reverse_compelment_seq = Sequence("CCCCGGGGAAAATTTTCCCCGGGGAAAATTTT")
+qual = Quality("FFFF####AAAAFFFFFFFFFFFFFFFFFFFF")
+@test  seq == same_seq
+@test seq == -reverse_seq
+@test seq == !compelment_seq
+@test seq == ~reverse_compelment_seq
+@test seq == -(-seq)
+@test seq == !(!seq)
+@test seq == ~(~seq)
+@test reverse_compelment_seq == !(-seq)
+@test reverse_compelment_seq == -(!seq)
