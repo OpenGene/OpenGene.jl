6acb6557b0393820e0005f38f0eb2dcbfe6dbbfb
Shifu Chen <chen@haplox.com>
2015-12-21 21:31:58 +0800

    add gz file reading test

--- test/IO/IO.jl
+++ test/IO/IO.jl
@@ -1,2 +1,19 @@
 
-# TODO: add tests for fastq gz files read/write
+# compare FastqRead from gz files and not gz files
+function test_gz_reading()
+    data_dir = Pkg.dir("OpenGene") * "/test/testdata/"
+    # open the gz file
+    gzstream = fastq_open(data_dir * "R1.fq.gz")
+    # open the fq file, whose content is same as the gz file
+    fqstream = fastq_open(data_dir * "R1.fq")
+
+    while (fq = fastq_read(fqstream)) != false
+        gzfq = fastq_read(gzstream)
+        if fq.sequence != gzfq.sequence || fq.name != gzfq.name || fq.quality.qual != gzfq.quality.qual
+            return false
+        end
+    end
+    return true
+end
+
+@test test_gz_reading()
