39a10ad0237f1eef47907f912b528e3c92b270a5
Shifu Chen <chen@haplox.com>
2016-03-04 22:08:02 +0800

    update gencode test case

--- test/Reference/Reference.jl
+++ test/Reference/Reference.jl
@@ -8,6 +8,11 @@ function test_gencode()
     if g["gene"]!="DDX11L1" || g["number"]!=1 || g["type"]!="intron"
         return false
     end
+    genes = gencode_genes(index, "TP53")
+    g = genes[1]
+    if g.name!="TP53" || g.chr!="chr17" || g.start_pos!=7565097 || g.end_pos!=7590856
+        return false
+    end
     return true
 end
 
