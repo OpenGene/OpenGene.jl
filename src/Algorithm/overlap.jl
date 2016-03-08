673bbb84c92da9d4f7b66eea76f3bc8cd04cc8b9
Shifu Chen <chen@haplox.com>
2016-03-08 23:32:50 +0800

    handle the case that insert DNA is shorter than read length and trimming is not clean

--- src/Algorithm/overlap.jl
+++ src/Algorithm/overlap.jl
@@ -11,6 +11,10 @@ function overlap(r1::Sequence, r2::Sequence)
     len2 = length(r2)
     reverse_r2 = ~r2
 
+    overlaped = false
+    overlap_len = 0
+    offset = 0
+    distance = 0
     # a match of less than 5 is considered as unconfident
     for offset = 0:len1-5
         # the overlap length of r1 & r2 when r2 is move right for offset
@@ -26,10 +30,33 @@ function overlap(r1::Sequence, r2::Sequence)
             next_overlap_len = min(len1, (next + len2 )) - next
             distance2 = edit_distance(r1.seq[next+1 : next+next_overlap_len], reverse_r2.seq[1 : next_overlap_len])
             if distance <= distance2
-                return (offset, overlap_len, distance)
+                overlaped = true
+                break
             end
         end
     end
+
+    if overlaped && offset == 0
+        # check if distance can get smaller if offset become negative
+        # this only happens when insert DNA is shorter than sequencing read length, and some adapter/primer is sequenced and not trimmed cleanly
+        # we go reversely
+        for offset = 0:-1:-min(len1, len2)
+            # the overlap length of r1 & r2 when r2 is move right for offset
+            overlap_len = min(len1,  len2- abs(offset))
+            distance = edit_distance(r1.seq[1:overlap_len], reverse_r2.seq[-offset + 1 : -offset + overlap_len])
+            if distance <= distance_threshold(overlap_len)
+                next = offset - 1
+                next_overlap_len = min(len1,  len2- abs(next))
+                distance2 = edit_distance(r1.seq[1:next_overlap_len], reverse_r2.seq[-next + 1 : -next + next_overlap_len])
+                if distance <= distance2
+                    return (offset, overlap_len, distance)
+                end
+            end
+        end
+    elseif overlapped
+        return (offset, overlap_len, distance)
+    end
+
     return (0,0,0)
 end
 
@@ -41,12 +68,18 @@ function overlap(pair::FastqPair)
     return overlap(pair.read1, pair.read2)
 end
 
-function simple_merge(r1::Sequence, r2::Sequence, overlap)
+function simple_merge(r1::Sequence, r2::Sequence, overlap, offset = 0)
     reverse = ~r2
-    if overlap < length(r2)
-        return Sequence(r1.seq * reverse.seq[overlap+1 : end])
+    if offset >= 0
+        if overlap < length(r2)
+            return Sequence(r1.seq * reverse.seq[overlap+1 : end])
+        else
+            return r1
+        end
     else
-        return r1
+        # offset is negative means insert DNA is shorter than read length
+        # just shift the junk bases in the tail of r1
+        return Sequence(r1.seq[1:overlap])
     end
 end
 
@@ -56,6 +89,6 @@ function try_merge(r1::Sequence, r2::Sequence; distance_threshold = 2, overlap_t
     if overlap_len < overlap_threshold || distance > distance_threshold
         return false
     else
-        return simple_merge(r1, r2, overlap_len)
+        return simple_merge(r1, r2, overlap_len, offset)
     end
 end
