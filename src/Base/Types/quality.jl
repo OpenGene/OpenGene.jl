c0f884112ec08674fd1bd66ea426fc2e9d11cc0c
Shifu Chen <chen@haplox.com>
2016-03-17 14:48:34 +0800

    add endof

--- src/Base/Types/quality.jl
+++ src/Base/Types/quality.jl
@@ -8,6 +8,7 @@ Base.sizeof(q::Quality) = sizeof(q.qual)
 Base.getindex(q::Quality, i::Int64) = getindex(q.qual, i)
 Base.getindex(q::Quality, r::UnitRange{Int64}) = Quality(getindex(q.qual, r))
 Base.getindex(q::Quality, indx::AbstractArray{Int64,1}) = Quality(getindex(q.qual, indx))
+Base.endof(q::Quality) = length(q)
 Base.reverse(q::Quality) = Quality(reverse(q.qual))
 ==(s1::Quality, s2::Quality) = s1.qual == s2.qual
 -(s1::Quality) = Quality(reverse(s1.qual))
