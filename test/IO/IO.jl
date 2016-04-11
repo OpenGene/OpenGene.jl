724bc7fc3758dbbfbb004ab7711c79deadfbbb05
Shifu Chen <chen@haplox.com>
2016-04-11 21:59:09 +0800

    add test case for fastq writing

--- test/IO/IO.jl
+++ test/IO/IO.jl
@@ -1,6 +1,6 @@
 
 # compare FastqRead from gz files and not gz files
-function test_gz_reading()
+function test_fastq_gz_reading()
     data_dir = Pkg.dir("OpenGene") * "/test/testdata/fq/"
     # open the gz file
     gzstream = fastq_open(data_dir * "R1.fq.gz")
@@ -16,6 +16,34 @@ function test_gz_reading()
     return true
 end
 
-@test test_gz_reading()
+function test_fastq_writing()
+    data_dir = Pkg.dir("OpenGene") * "/test/testdata/fq/"
+
+    # open the fq file, whose content is same as the gz file
+    instream = fastq_open(data_dir * "R1.fq")
+    outstream = fastq_open(data_dir * "R1.out.fq", "w")
+
+    while (fq = fastq_read(instream)) != false
+        fastq_write(outstream, fq)
+    end
+    close(outstream)
+
+    instream1 = fastq_open(data_dir * "R1.fq")
+    instream2 = fastq_open(data_dir * "R1.out.fq")
+
+    while (fq1 = fastq_read(instream1)) != false
+        fq2 = fastq_read(instream2)
+        if fq1.sequence != fq2.sequence || fq1.name != fq2.name || fq1.quality.qual != fq2.quality.qual
+            return false
+        end
+    end
+
+    rm(data_dir * "R1.out.fq")
+
+    return true
+end
+
+@test test_fastq_gz_reading()
+@test test_fastq_writing()
 
 include("vcf.jl")
