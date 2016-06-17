ed43acf25faccfc9855ee7db0fce3f57c86a8aa9
Shifu Chen <chen@haplox.com>
2016-06-17 22:10:31 +0800

    add VCF operation testcases

--- test/IO/vcf.jl
+++ test/IO/vcf.jl
@@ -6,4 +6,13 @@ function test_reading_vcf()
     return true
 end
 
+function test_vcf_operation()
+    v1 = vcf_read(Pkg.dir("OpenGene") * "/test/testdata/vcf/cmp-test-a.vcf")
+    v2 = vcf_read(Pkg.dir("OpenGene")* "/test/testdata/vcf/cmp-test-b.vcf")
+    minus = v1 - v2
+    intersect = v1 * v2
+    return length(minus) == 1 && length(intersect) == length(v1)-1
+end
+
 @test test_reading_vcf()
+@test test_vcf_operation()
